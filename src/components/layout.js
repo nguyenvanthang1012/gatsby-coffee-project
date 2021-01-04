a51332ce3f0e9221efd526c0d29dbbbc856358a5
nguyenvanthang1012 <vaichicomotmi@gmail.com>
2021-01-04 20:46:13 +0700

    navbar added as well as basic css

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,9 +9,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import "./bootstrap.min.css"
 import "./layout.css"
+import Navbar from './Globals/Navbar'
 
-
-const Layout = ({ children }) => <>{children}</>;
+const Layout = ({ children }) => 
+ <>
+  <Navbar/>
+  {children}
+ </>;
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,

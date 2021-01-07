a3de9121f80d1f8bb8f517f874e75f82fb73f177
nguyenvanthang1012 <vaichicomotmi@gmail.com>
2021-01-07 10:34:06 +0700

    Snipcart-setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
+import Products from "../components/Home/Products"
+import Contact from "../components/Home/Contact"
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home"/>
@@ -12,6 +15,9 @@ const IndexPage = ({data}) => (
      title="regular joe's" 
      styleClass="default-background" />
     <Info />
+    <Menu items={data.menu}/>
+    <Products/>
+    <Contact/>
   </Layout>
 )
 
@@ -24,6 +30,24 @@ export const query = graphql`
     }
    }
  }
+ menu:allContentfulCoffeeItem{
+  edges{
+    node{
+      id
+      title
+      description{
+        description
+      }
+      price
+      category
+      image{
+        fixed(width:50,height:50){
+         ...GatsbyContentfulFixed_tracedSVG
+        }
+      }
+    }
+  }
+}
 }
 `
 
